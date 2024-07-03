6d56ba3adee5f6cbc69c2e0441239315acaacaee
Mazzocchi <nicolas.mazzocchi@live.fr>
2024-07-03 14:01:33 +0200

    fix top inf

--- src/main.cpp
+++ src/main.cpp
@@ -20,16 +20,16 @@ int main(int argc, char **argv) {
 
 	Automaton* A = new Automaton("samples/test6.txt");
 	A->print();
-	// Automaton* B = Automaton::safetyClosure(A, Inf);
-	// B->print();
+	Automaton* B = Automaton::safetyClosure(A, Inf);
+	B->print();
 
 	std::cout << A->isConstant(Inf) << std::endl;
-	// std::cout << B->isConstant(Inf) << std::endl;
+	std::cout << B->isConstant(Inf) << std::endl;
 	std::cout << A->isSafe(Inf) << std::endl;
 	std::cout << A->isLive(Inf) << std::endl;
 
-	delete A;
-	// delete B;
+	//delete A;
+	//delete B;
 
 	// debug_test();
 
