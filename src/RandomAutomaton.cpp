ce1902519620cf370ca1452aee981d7885ca54f8
Marek Chalupa <mchqwerty@gmail.com>
2024-07-09 20:52:05 +0200

    Add tests

--- src/RandomAutomaton.cpp
+++ src/RandomAutomaton.cpp
@@ -1,5 +1,6 @@
 #include <random>
 #include <map>
+#include <vector>
 
 #include "Automaton.h"
 #include "State.h"
@@ -33,6 +34,9 @@ Automaton *Automaton::randomAutomaton(const std::string& name,
   if (edges_num == 0) {
     std::uniform_int_distribution<unsigned> rand(states_num / 2.0, states_num*states_num);
     edges_num = rand(reng);
+    if (edges_num == 0) {
+      edges_num = states_num;
+    }
   }
 
   auto alphabet_size = alphabet->size();
@@ -44,21 +48,17 @@ Automaton *Automaton::randomAutomaton(const std::string& name,
     new_alphabet->insert(n, new Symbol(alphabet->at(n)));
   }
 
-  // generate states
-  MapArray<State*>* states = new MapArray<State*>(states_num); 
-  for (auto n = 0U; n < states_num; ++n) {
-    auto *state = new State(std::string("q") + std::to_string(n),
-                            alphabet_size,
-                            min_weight,
-                            max_weight);
-    states->insert(n, state);
-  }
-
   // generate edges
   std::uniform_real_distribution<weight_t> rand_weight(min_weight, max_weight);
-  std::uniform_int_distribution<unsigned> rand_state(0, states_num);
-  std::uniform_int_distribution<unsigned> rand_symbol(0, alphabet_size);
+  std::uniform_int_distribution<unsigned> rand_state(0, states_num - 1);
+  std::uniform_int_distribution<unsigned> rand_symbol(0, alphabet_size - 1);
   std::map<weight_t, Weight *> weights_register;
+
+  weight_t real_min = max_weight;
+  weight_t real_max = min_weight;
+
+  std::vector<std::tuple<unsigned, unsigned, weight_t, unsigned>> tmpedges;
+  tmpedges.reserve(edges_num);
 
   while (edges_num > 0) {
     auto src_id = rand_state(reng);
@@ -66,22 +66,27 @@ Automaton *Automaton::randomAutomaton(const std::string& name,
     auto weight = rand_weight(reng);
     auto symbol = rand_symbol(reng);
 
-    State *src = states->at(src_id);
-    State *dest = states->at(dest_id);
-
     auto *W = weights_register[weight];
     if (W == nullptr) {
       W = new Weight(weight);
       weights_register[weight] = W;
     }
 
-		Edge *edge = new Edge(new_alphabet->at(symbol), W, src, dest);
-		src->addSuccessor(edge);
-		dest->addPredecessor(edge);
+    if (weight < real_min)
+      real_min = weight;
+    if (weight > real_max)
+      real_max = weight;
+
+    tmpedges.push_back({src_id, dest_id, weight, symbol});
 
     --edges_num;
   }
 
+  ///////////////////////////////////////////////////////////////////
+  // Generate the automaton
+  ///////////////////////////////////////////////////////////////////
+  
+  // generate weights
   MapArray<Weight*>* weights = new MapArray<Weight*>(weights_register.size()); 
   unsigned n = 0;
   for (auto& it : weights_register) {
@@ -89,5 +94,27 @@ Automaton *Automaton::randomAutomaton(const std::string& name,
     ++n;
   }
 
-  return new Automaton(name, new_alphabet, states, weights, min_weight, max_weight, states->at(0));
+  // generate states
+  MapArray<State*>* states = new MapArray<State*>(states_num); 
+  for (auto n = 0U; n < states_num; ++n) {
+    auto *state = new State(std::string("q") + std::to_string(n),
+                            alphabet_size,
+                            real_min,
+                            real_max);
+    states->insert(n, state);
+  }
+
+  // generate edges
+  for (auto& tmpedge : tmpedges) {
+    State *src = states->at(std::get<0>(tmpedge));
+    State *dest = states->at(std::get<1>(tmpedge));
+    Weight *W = weights_register[std::get<2>(tmpedge)];
+    assert(W != nullptr);
+
+    Edge *edge = new Edge(new_alphabet->at(std::get<3>(tmpedge)), W, src, dest);
+    src->addSuccessor(edge);
+    dest->addPredecessor(edge);
+  }
+
+  return new Automaton(name, new_alphabet, states, weights, real_min, real_max, states->at(0));
 }
