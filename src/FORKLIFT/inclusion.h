6cd33dea75118da03b3f9962363749a55f6d1d10
Marek Chalupa <mchqwerty@gmail.com>
2024-07-12 15:59:48 +0200

    build: leave the opt macros be defined only by the Makefile or cmake

--- src/FORKLIFT/inclusion.h
+++ src/FORKLIFT/inclusion.h
@@ -5,13 +5,6 @@
 #include "../Automaton.h"
 
 
-// These are defined by CMake or Makefile.legacy. Uncomment only if you want
-// to (temporarily, e.g., for debugging without changing makefiles)
-// hard-code these.
-// #define INCLUSION_SCC_SEARCH_ACTIVE
- #define CONTEXT_REDUNDANCY_ACTIVE
-
-
 bool inclusion (const Automaton* A, const Automaton* B);
 void debug_test();
 void debug_test2();
