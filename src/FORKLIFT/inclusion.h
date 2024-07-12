15c265beabaea7ac2fef292dcbd3305f58c69591
Mazzocchi <nicolas.mazzocchi@live.fr>
2024-07-12 09:35:33 +0200

    test E

--- src/FORKLIFT/inclusion.h
+++ src/FORKLIFT/inclusion.h
@@ -8,7 +8,7 @@
 // These are defined by CMake. Uncomment only if you want
 // to hard-code these (or you don't use CMake to create makefiles)
 // #define INCLUSION_SCC_SEARCH_ACTIVE
-#define CONTEXT_REDUNDANCY_ACTIVE
+//#define CONTEXT_REDUNDANCY_ACTIVE
 
 
 bool inclusion (const Automaton* A, const Automaton* B);
