1956e176743980e3445c470fb96ea1095e482628
Marek Chalupa <mchqwerty@gmail.com>
2024-10-16 22:26:36 +0200

    Update scripts

--- experiments/run-inclusion.sh
+++ experiments/run-inclusion.sh
@@ -12,8 +12,12 @@ mkdir -p results
 
 for VALUE_FUN in Sup LimSup; do
 for S in 2-sym; do
-	echo "Running experiments for $VALUE_FUN with $S alphabet"
-	python3 "$DIR/run-inclusion.py" --dir $DIR/automata-$S --out results/$VALUE_FUN-$S --value-fun $VALUE_FUN --bindir="$BUILD/experiments" $@
-	python3 "$DIR/run-inclusion.py" --dir $DIR/automata-$S --out results/$VALUE_FUN-$S-no-scc --value-fun $VALUE_FUN --bindir="$BUILD_NO_SCC/experiments" $@
+	echo " -- Running experiments for $VALUE_FUN with $S alphabet --"
+	python3 "$DIR/run-inclusion.py" --dir $DIR/automata-$S --out "results/$VALUE_FUN-$S.csv" --value-fun $VALUE_FUN --bindir="$BUILD/experiments" $@
+	echo "## results stored into 'results/$VALUE_FUN-$S.csv'"
+
+	echo " -- Running experiments for $VALUE_FUN with $S alphabet, no SCC optimization --"
+	python3 "$DIR/run-inclusion.py" --dir $DIR/automata-$S --out "results/$VALUE_FUN-$S-no-scc.csv" --value-fun $VALUE_FUN --bindir="$BUILD_NO_SCC/experiments" $@
+	echo "## results stored into 'results/$VALUE_FUN-$S-no-scc.csv'"
 done
 done
