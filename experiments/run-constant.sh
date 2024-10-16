1909a5fdd9076ed2aae6e141d0423800fd30cc93
Marek Chalupa <mchqwerty@gmail.com>
2024-10-16 22:27:02 +0200

    add run-constant.sh

---
+++ experiments/run-constant.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+DIR=$(dirname $0)/
+
+# we assume the docker build, adjust if necessary
+BUILD=$(dirname $0)/../build
+BUILD_NO_SCC=$(dirname $0)/../build-no-scc
+
+mkdir -p results
+
+for VALUE_FUN in LimInfAvg LimSupAvg; do
+for S in 2-sym; do
+	echo ""
+	echo " -- Running experiments for $VALUE_FUN with $S alphabet --"
+	python3 "$DIR/run-constant.py" --dir $DIR/automata-$S --out "results/$VALUE_FUN-$S-constant.csv" --value-fun $VALUE_FUN $@
+	echo "## results stored into 'results/$VALUE_FUN-$S-constant.csv'"
+done
+done
