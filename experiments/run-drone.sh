d45d3137bb083969b1ad6404ad3ee19d9fbf5de4
Marek Chalupa <mchqwerty@gmail.com>
2024-10-16 23:09:49 +0200

    Update drone

--- experiments/run-drone.sh
+++ experiments/run-drone.sh
@@ -9,7 +9,12 @@ BUILD=$(dirname $0)/../build
 BUILD=$(dirname $0)/../
 
 mkdir -p results
+echo  "" > "results/drone.txt"
 
+for i in `seq 1 3`; do
+echo "================================================================" | tee -a "results/drone.txt"
+echo " trial $i" | tee -a "results/drone.txt"
+echo "================================================================" | tee -a "results/drone.txt"
 for SD in 1 5; do
 	echo ""
 	echo " -- Running experiments for sd=$SD --"
@@ -20,7 +25,8 @@ for SD in 1 5; do
 	$BUILD/quak $DIR/drone-monitor.txt monitor Avg forces.txt > monitor-sd$SD.log
 	$BUILD/quak $DIR/drone-monitor.txt monitor Avg forces-smooth.txt > monitor-sd$SD-smooth.log
 	SCORE=$(tail -n 2 monitor-sd$SD.log | head -n 1 | cut -d ' ' -f 3)
-	echo "Monitor score random: $SCORE"
+	echo "Monitor score random: $SCORE" | tee -a "results/drone.txt"
 	SCORE=$(tail -n 2 monitor-sd$SD-smooth.log | head -n 1  | cut -d ' ' -f 3)
-	echo "Monitor score smoothed: $SCORE"
+	echo "Monitor score smoothed: $SCORE" | tee -a "results/drone.txt"
+done
 done
