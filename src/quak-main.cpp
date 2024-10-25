a8698a916adb9a14ec4f100ae17c2923ce84d5a2
egesarac <egesarac@gmail.com>
2024-10-25 12:34:36 +0200

    memory issue testing

--- src/quak-main.cpp
+++ src/quak-main.cpp
@@ -5,7 +5,6 @@
 #include <string>
 #include <algorithm>
 #include <iomanip>
-
 #include "FORKLIFT/inclusion.h"
 #include "Automaton.h"
 #include "Monitor.h"
@@ -69,65 +68,114 @@
 //     return 0;
 // }
 
+std::string val2str(value_function_t f) {
+    if (f == Inf) {
+        return "Inf";
+    }
+    if (f == Sup) {
+        return "Sup";
+    }
+    if (f == LimInf) {
+        return "LimInf";
+    }
+    if (f == LimSup) {
+        return "LimSup";
+    }
+    if (f == LimInfAvg) {
+        return "LimInfAvg";
+    }
+    if (f == LimSupAvg) {
+        return "LimInfAvg";
+    }
+    
+    return "";
+}
 
 int main() {
     std::vector<int> states = {2,3,4,5,6,7,8};
     std::vector<int> letters = {2,3,4};
+    // std::vector<int> states = {2};
+    // std::vector<int> letters = {2};
+    std::vector<value_function_t> valueFunctions = {Inf, Sup, LimInf, LimSup};
+    // std::vector<value_function_t> valueFunctions = {Inf, Sup, LimInf, LimSup, LimInfAvg, LimSupAvg};
 
-    // std::string directory = "../samples/";
     std::string directory = "../samples/randBig";
     
-    for (int num_states : states) {
-        for (int num_letters : letters) {
-            for (int id = 1; id <= 10000; ++id) {
-                std::ostringstream filename;
-                filename << num_states << "_" << num_letters << "_" 
-                         << std::setw(4) << std::setfill('0') << id << ".txt";
-                
-                std::cout << "Processing file: " << filename.str() << std::endl;
-                
-                // Automaton* A = new Automaton(directory + "/" + "ainf.txt");
-                Automaton* A = new Automaton(directory + "/" + filename.str());
-                // A->print();
+    for (value_function_t f : valueFunctions) {
+        for (int num_states : states) {
+            for (int num_letters : letters) {
+                for (int id = 1; id <= 10000; ++id) {
+                    std::ostringstream filename;
+                    filename << num_states << "_" << num_letters << "_" 
+                            << std::setw(4) << std::setfill('0') << id << ".txt";
+                    
+                    std::cout << "Processing automaton: " <<  val2str(f) << " " << filename.str() << std::endl;
+                    
+                    Automaton* A = new Automaton(directory + "/" + filename.str());
+                    A->print();
 
-                // Automaton* B = Automaton::safetyClosure(A, Sup);
-                // bool flag = B->isSafe(LimSup);
+                    // testing the top value witness
+                    /*
+                    UltimatelyPeriodicWord* witness = new UltimatelyPeriodicWord();
+                    weight_t top = A->getTopValue(f, &witness);
+                    weight_t valWitness = A->computeValue(f, witness);
 
-                // delete B;
-                // delete A;
+                    delete witness->prefix;
+                    delete witness->cycle;
+                    delete witness;
+                    delete A;
 
-                // if (!flag) {
-                //     std::cout << "ERROR" << std::endl;
-                // }
+                    if (top != valWitness) {
+                        std::cout << "ERROR" << std::endl;
+                        return -1;
+                    }
+                    */
+
+                    // testing the universality witness (which calls inclusion)
+                    UltimatelyPeriodicWord* witness = new UltimatelyPeriodicWord();
+                    weight_t top = A->getTopValue(f);
+                    bool flag = A->isUniversal(f, top, &witness);
+
+                    weight_t valWitness = top - 1;
+                    if (!flag) {
+                        valWitness = A->computeValue(f, witness);
+                    }
+
+                    delete witness;
+                    delete A;
+
+                    if (valWitness >= top) {
+                        std::cout << "ERROR" << std::endl;
+                        return -1;
+                    }
 
 
-                // Automaton* AA = Automaton::toLimSup(A, Inf);
-                // AA->print();
-                // UltimatelyPeriodicWord* witness = new UltimatelyPeriodicWord();
-                // weight_t top = AA->getTopValue(LimSup, &witness);
-                // std::cout << "Top:" << top << std::endl;
-                // std::cout << "Witness: " << witness->prefix->toString() << " . " << witness->cycle->toString() << std::endl;
-                // weight_t valWitness = A->computeValue(Inf, witness);
-                // std::cout << "Value: " << valWitness << std::endl;
-                // delete witness->prefix;
-                // delete witness->cycle;
-                // delete witness;
-                // delete AA;
-                // delete A;
+                    // Automaton* B = Automaton::safetyClosure(A, Sup);
+                    // bool flag = B->isSafe(LimSup);
 
-                UltimatelyPeriodicWord* witness = new UltimatelyPeriodicWord();
-                weight_t top = A->getTopValue(Inf, &witness);
-                // std::cout << "Top:" << top << std::endl;
-                // std::cout << "Witness: " << witness->prefix->toString() << " . " << witness->cycle->toString() << std::endl;
-                weight_t valWitness = A->computeValue(Inf, witness);
-                // std::cout << "Value: " << valWitness << std::endl;
-                delete witness->prefix;
-                delete witness->cycle;
-                delete witness;
-                delete A;
+                    // delete B;
+                    // delete A;
 
-                if (top != valWitness) {
-                    std::cout << "ERROR" << std::endl;
+                    // if (!flag) {
+                    //     std::cout << "ERROR" << std::endl;
+                    // }
+
+
+                    // Automaton* AA = Automaton::toLimSup(A, Inf);
+                    // AA->print();
+                    // UltimatelyPeriodicWord* witness = new UltimatelyPeriodicWord();
+                    // weight_t top = AA->getTopValue(LimSup, &witness);
+                    // std::cout << "Top:" << top << std::endl;
+                    // std::cout << "Witness: " << witness->prefix->toString() << " . " << witness->cycle->toString() << std::endl;
+                    // weight_t valWitness = A->computeValue(Inf, witness);
+                    // std::cout << "Value: " << valWitness << std::endl;
+                    // delete witness->prefix;
+                    // delete witness->cycle;
+                    // delete witness;
+                    // delete AA;
+                    // delete A;
+
+
                 }
             }
         }
