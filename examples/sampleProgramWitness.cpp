941a89d289cc6cb5ebe900daee222aacde98de30
Marek Chalupa <mchqwerty@gmail.com>
2024-10-25 13:40:56 +0200

    Update the example program

--- examples/sampleProgramWitness.cpp
+++ examples/sampleProgramWitness.cpp
@@ -19,32 +19,28 @@ int main() {
     Automaton* C = Automaton::livenessComponent_prefixIndependent(A, LimInfAvg);
     C->print();
 
-    UltimatelyPeriodicWord* witEmp = new UltimatelyPeriodicWord();
+    UltimatelyPeriodicWord* witEmp;
     bool flagNonEmp = A->isNonEmpty(LimInfAvg, 5, &witEmp);
 
     if (flagNonEmp) {
         weight_t valWitness = A->computeValue(LimInfAvg, witEmp);
-        std::cout << "Witness to non-emptiness of A w.r.t. value 5: " << witEmp->prefix->toString() << " . " << witEmp->cycle->toString() << std::endl;
+        std::cout << "Witness to non-emptiness of A w.r.t. value 5: " << witEmp->toString() << std::endl;
         std::cout << "Value: " << valWitness << std::endl;
     }
 
-    delete witEmp->prefix;
-    delete witEmp->cycle;
     delete witEmp;
 
-    UltimatelyPeriodicWord* witSafe = new UltimatelyPeriodicWord();
+    UltimatelyPeriodicWord* witSafe;
     bool flagSafe = A->isSafe(LimInfAvg, &witSafe);
 
     if (!flagSafe) {
         weight_t valWitness = A->computeValue(LimInfAvg, witSafe);
         weight_t valWitnessClosure = B->computeValue(LimInfAvg, witSafe);
-        std::cout << "Witness to non-safety of A: " << witSafe->prefix->toString() << " . " << witSafe->cycle->toString() << std::endl;
+        std::cout << "Witness to non-safety of A: " << witSafe->toString() << std::endl;
         std::cout << "Value on A: " << valWitness << std::endl;
         std::cout << "Value on SafetyClosure(A): " << valWitnessClosure << std::endl;
     }
 
-    delete witSafe->prefix;
-    delete witSafe->cycle;
     delete witSafe;
     
     delete C;
